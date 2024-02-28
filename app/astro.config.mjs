bf5a687cb4daa5629e1f7db747c0dd81de1ed3f1
André Lademann <alademann@thinkport.digital>
2024-02-28 21:56:46 +0100

    🚑 fix: Update base URL in astro.config for GitHub Pages deployment

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import {defineConfig} from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
 	build: {
-		site: 'https://nullzueins.github.io/tagesmutter-peters-de',
+		site: 'https://nullzueins.github.io/tagesmutter-peters-de/',
+		base: '/tagesmutter-peters-de/'
 	},
 	integrations: [tailwind()],
 	output: "static",
