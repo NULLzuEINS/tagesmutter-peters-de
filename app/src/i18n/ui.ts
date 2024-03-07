d630ef1ae7f2a1ccefb23d59025417d22208c7ba
André Lademann <alademann@thinkport.digital>
2024-03-07 13:04:03 +0100

    feat: Add multilangual content

---
+++ app/src/i18n/ui.ts
@@ -0,0 +1,40 @@
+export const languages = {
+	de: 'Deutsch',
+	en: 'English',
+	fr: 'Unkrainian',
+}
+
+export const defaultLang = 'de'
+
+export const ui = {
+	de: {
+		'nav.environment': 'Einrichtung',
+		'nav.about': 'Über mich',
+		'nav.contact': 'Kontakt',
+		'nav.opener': 'Das Menü öffnen',
+		'document.title': 'Tagesmutter in Leipzig',
+		'document.subTitle': 'Ausgebildete Tagesmutter und Ergotherapeutin in der Südvorstadt von Leipzig',
+		'header.childdaycare': 'Tagesmutter',
+		'footer.imprint': 'Impressum'
+	},
+	en: {
+		'nav.environment': 'Environment',
+		'nav.about': 'About Me',
+		'nav.contact': 'Contact',
+		'nav.opener': 'Open the Menu',
+		'document.title': 'Daycare Provider in Leipzig',
+		'document.subTitle': 'Trained Daycare Provider and Occupational Therapist in the South District of Leipzig',
+		'header.childdaycare': 'Childcare',
+		'footer.imprint': 'Imprint'
+	},
+	ua: {
+		'nav.environment': 'Оточення',
+		'nav.about': 'Про мене',
+		'nav.contact': 'Контакти',
+		'nav.opener': 'Відкрити меню',
+		'document.title': 'Дитячий садок у Лейпцигу',
+		'document.subTitle': 'Навчена вихователька та ерготерапевтка в південному районі Лейпцигу',
+		'header.childdaycare': 'Дитячий садок',
+		'footer.imprint': 'Імпресум',
+	},
+} as const
